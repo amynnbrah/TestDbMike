4b0a3c4c2cc1d9f31749ac70e05a9c5b5f935bdb
Fucci Im In <53264025+amynnbrah@users.noreply.github.com>
2024-05-05 12:06:29 -0700

    Update script.sh
    Fix: removed extra useless grep line

--- script.sh
+++ script.sh
@@ -10,7 +10,6 @@ grep -in "testdb" file_utf8_modified.txt
 
 echo -e "\ngrep lines containing 'data 1' from main text file:\n"
 grep -in "data1" file_utf8.txt
-grep -in "data1" file_utf8_modified.txt
 awk '{gsub(/DATA1/, "PRIMARY"); print}' file_utf8.txt >> file_utf8_modified.txt
 echo -e "\nModified text lines after awk(DATA1 > PRIMARY):"
 grep -in "primary" file_utf8_modified.txt | awk -F ":" '{print $1 ":" $2}'
