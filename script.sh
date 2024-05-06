ba9a8f41e31531ef6566f618d8a081923b3e7aca
Fucci Im In <53264025+amynnbrah@users.noreply.github.com>
2024-05-05 20:50:37 -0700

    Fix: had 2 occurrences of AWK command printing the file twice

--- script.sh
+++ script.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 echo -e "\nhello test bash\n"
+#clear the target file
 truncate -s 0 file_utf8_modified.txt
+
+#get occurrences of "Mike" from origin
 echo "grep lines containing 'Mike' from main text file:"
 grep -in "Mike" file_utf8.txt
-awk '{gsub(/Mike/, "TestDB"); print}' file_utf8.txt >> file_utf8_modified.txt
-echo -e "\nModified text lines after awk(Mike > TestDB):"
-grep -in "testdb" file_utf8_modified.txt
-
-
+#get occurrences of "DATA1" from origin 
 echo -e "\ngrep lines containing 'data 1' from main text file:\n"
 grep -in "data1" file_utf8.txt
-awk '{gsub(/DATA1/, "PRIMARY"); print}' file_utf8.txt >> file_utf8_modified.txt
-echo -e "\nModified text lines after awk(DATA1 > PRIMARY):"
+
+#switch the original occurrences to the desired ones 
+awk '/Mike/ {gsub(/Mike/, "TestDB"); print}
+/DATA1/ {gsub(/DATA1/, "PRIMARY"); print}
+' file_utf8.txt >> file_utf8_modified.txt
+#check changes Mike > TestDB
+echo -e "\nModified text lines after awk(Mike > TestDB):\n"
+grep -in "testdb" file_utf8_modified.txt
+#check changes DATA1 > PRIMARY 
+echo -e "\nModified text lines after awk(DATA1 > PRIMARY):\n"
 grep -in "primary" file_utf8_modified.txt | awk -F ":" '{print $1 ":" $2}'
+
+
+
+
