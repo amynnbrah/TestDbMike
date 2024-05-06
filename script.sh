9025ae04c96fd2d38ddfe6c6c0b2aef5e077969f
Fucci Im In <kimou998@gmail.com>
2024-05-05 21:11:38 -0700

    feat: final commit with bug fixes

--- script.sh
+++ script.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 echo -e "\nhello test bash\n"
+#clear the target file
+truncate -s 0 file_utf8_modified.txt
 
-awk '{gsub(/Mike/, "TestDB"); print}' file_utf8.txt >> file_utf8_modified.txt
-echo -e "\nmike >> testDB"
-awk '{gsub(/DATA1/, "PRIMARY"); print}' file_utf8.txt >> file_utf8_modified.txt
-echo -e "\nDATA1 >> PRIMARY"
+#get occurrences of "Mike" from origin
+echo "grep lines containing 'Mike' from main text file:"
+grep -in "Mike" file_utf8.txt
+#get occurrences of "DATA1" from origin 
+echo -e "\ngrep lines containing 'data 1' from main text file:\n"
+grep -in "data1" file_utf8.txt
+
+#switch the original occurrences to the desired ones 
+awk '
+/Mike/ {gsub(/Mike/, "TestDB")}
+/DATA1/ {gsub(/DATA1/, "PRIMARY")}
+{print}
+' file_utf8.txt > file_utf8_modified.txt
+
+#check changes Mike > TestDB
+echo -e "\nModified text lines after awk(Mike > TestDB):\n"
+grep -in "testdb" file_utf8_modified.txt
+#check changes DATA1 > PRIMARY 
+echo -e "\nModified text lines after awk(DATA1 > PRIMARY):\n"
+grep -in "primary" file_utf8_modified.txt | awk -F ":" '{print $1 ":" $2}'
