ecff6583dc93dd2c722355da008340ddccd1d1ea
Fucci Im In <kimou998@gmail.com>
2024-05-07 12:09:36 -0700

    feat: added new feature to read user input letting them pick a file name

--- script.sh
+++ script.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+#default target file
+TARGETFILE=file_utf8_modified.txt 
+#hello test
 echo -e "\nhello test bash\n"
 #clear the target file
 truncate -s 0 file_utf8_modified.txt
+
+#give the user a choice between using default file or creating a new one
 echo "press 1 if you want to create a new file"
-echo "press 2 if you want to modify the existing file"
+echo "press any other button if you want to modify the existing file"
 read CHOICE
+
+#if user creates a new file
 if [[ $CHOICE -eq 1 ]]
 then
+#get file name
 echo "please enter desired new file name"
 read TARGETFILE
-else
-TARGETFILE=file_utf8_modified.txt 
+#make sure the file is clear
+truncate -s 0 $TARGETFILE
+
 fi
 #get occurrences of "Mike" from origin
 echo "grep lines containing 'Mike' from main text file:"
-grep -in "Mike" file_utf8.txt
+grep -in --color=auto "Mike" file_utf8.txt
 #get occurrences of "DATA1" from origin 
-echo -e "\ngrep lines containing 'data 1' from main text file:\n"
-grep -in "data1" file_utf8.txt
+echo -e  "\ngrep lines containing 'data 1' from main text file:\n"
+grep -in --color=auto "data1" file_utf8.txt
 
 #switch the original occurrences to the desired ones 
 awk '
@@ -29,7 +38,7 @@ awk '
 
 #check changes Mike > TestDB
 echo -e "\nModified text lines after awk(Mike > TestDB):\n"
-grep -in "testdb" "$TARGETFILE"
+grep -in --color=auto "testdb" "$TARGETFILE"
 #check changes DATA1 > PRIMARY 
 echo -e "\nModified text lines after awk(DATA1 > PRIMARY):\n"
-grep -in "$TARGETFILE" | awk -F ":" '{print $1 ":" $2}'
+grep -in --color=auto "PRIMARY" "$TARGETFILE" 
