2ffe4dd899f4483ddeccb9fcb4bd76d7c393bd9a
Fucci Im In <53264025+amynnbrah@users.noreply.github.com>
2024-05-07 11:57:25 -0700

    Feat: added an interactive script to read the target file

--- script.sh
+++ script.sh
@@ -3,7 +3,16 @@
 echo -e "\nhello test bash\n"
 #clear the target file
 truncate -s 0 file_utf8_modified.txt
-
+echo "press 1 if you want to create a new file"
+echo "press 2 if you want to modify the existing file"
+read CHOICE
+if [[ $CHOICE -eq 1 ]]
+then
+echo "please enter desired new file name"
+read TARGETFILE
+else
+TARGETFILE=file_utf8_modified.txt 
+fi
 #get occurrences of "Mike" from origin
 echo "grep lines containing 'Mike' from main text file:"
 grep -in "Mike" file_utf8.txt
@@ -16,11 +25,11 @@ awk '
 /Mike/ {gsub(/Mike/, "TestDB")}
 /DATA1/ {gsub(/DATA1/, "PRIMARY")}
 {print}
-' file_utf8.txt > file_utf8_modified.txt
+' file_utf8.txt > "$TARGETFILE"
 
 #check changes Mike > TestDB
 echo -e "\nModified text lines after awk(Mike > TestDB):\n"
-grep -in "testdb" file_utf8_modified.txt
+grep -in "testdb" "$TARGETFILE"
 #check changes DATA1 > PRIMARY 
 echo -e "\nModified text lines after awk(DATA1 > PRIMARY):\n"
-grep -in "primary" file_utf8_modified.txt | awk -F ":" '{print $1 ":" $2}'
+grep -in "$TARGETFILE" | awk -F ":" '{print $1 ":" $2}'
